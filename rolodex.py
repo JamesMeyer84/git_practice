08746aca7c3e681f76a43769861c481fd929fa6b
Elliott <james.meyer84@gmail.com>
2021-04-05 17:20:44 -0500

    Add & Delete work; still needs rest of functionality & 'exceptions'

--- rolodex.py
+++ rolodex.py
@@ -32,27 +32,41 @@ with open(filename+'.csv','r') as rolodex:
   next(rolodex_contents)
   
   for entry in rolodex_contents:
-    print(entry)
+    new_entry(entry[0], entry[1], entry[2], entry[3], entry[4])
 
 
-print('Hello! What would you like to do today: Add, Delete, Update, or Display?')
+print("Please choose an action to perform:")
+print("--Enter 'Add', 'Delete', 'Update', 'Display', or 'Exit'")
 resp = input()
 
+while resp != 'Exit':
+  if resp == 'Add':
+    print('Please enter the new entry\'s name:')
+    new_name = input()
+    print('Please enter the new entry\'s phone number:')
+    new_phone = input()
+    print('Please enter new entry\'s address:')
+    new_address = input()
+    print('Please enter new entry\'s birthday:')
+    new_bday = input()
+    print('Please enter any notes:')
+    new_notes = input()
+    #New object creation
+    new_entry(new_name, new_phone, new_address, new_bday, new_notes)
+    print("Created new entry.  Will there be anything else?")
+    print("--Enter 'Add', 'Delete', 'Update', 'Display', or 'Exit'")
+    resp = input()
+  elif resp == 'Delete':
+    print('Please enter the name of the person whose entry you would like to delete:')
+    # Put a condition here for if there's not a matching entry
+    del_name = input()
+    del_key = 0
+    for id, name in entries.items():
+      if str(name) == del_name:
+        del_key = id
+    entries.pop(del_key)
+    print("Entry deleted.  Will there be anything else?")
+    print("--Enter 'Add', 'Delete', 'Update', 'Display', or 'Exit'")
+    resp = input()
 
-if resp == 'Add':
-  print('What is the new entry\'s name?')
-  new_name = input()
-  print('What is the new entry\'s phone number?')
-  new_phone = input()
-  print('Please enter new entry\'s address:')
-  new_address = input()
-  print('Please enter new entry\'s birthday:')
-  new_bday = input()
-  print('Please enter any notes:')
-  new_notes = input()
-  #New object creation
-  new_entry(new_name, new_phone, new_address, new_bday, new_notes)
-  print("Created new entry")
-
-
-#print(entries[1])
+print(entries)
